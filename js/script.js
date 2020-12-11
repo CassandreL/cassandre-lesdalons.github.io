4fc7edb353ea7dcbca1292ec44bbfbde96c24711
CassandreL <61794728+CassandreL@users.noreply.github.com>
2020-12-11 18:52:44 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+  $("slider")=getElementById('slider');
+  $("div1")=getElementById('div1');
+  $("div2")=getElementById('div2');
+
+  $("div1").hide();
+  $("div2").hide();
+
+  tab=new Array[div1,div2]
+
+  for (var i = 0; i < tab.length; i++) {
+    tab[i]
+  }
+})
